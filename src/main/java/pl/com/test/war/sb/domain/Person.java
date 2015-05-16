8130866bb29301ac8cd52f17e11bafdaea9e0b85
Administrator <Administrator@10.27.32.100>
2015-05-16 14:12:53 +0200

    Json i XML

---
+++ src/main/java/pl/com/test/war/sb/domain/Person.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.test.war.sb.domain;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Administrator
+ */
+@XmlRootElement
+public class Person {
+    
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    
+}
--- src/main/java/pl/com/test/war/sb/rest/MyController.java
+++ src/main/java/pl/com/test/war/sb/rest/MyController.java
@@ -3,8 +3,10 @@ package pl.com.test.war.sb.rest;
  * Copyright 2015-05-16 the original author or authors.
  */
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.com.test.war.sb.domain.Person;
 
 /**
  * @author Adrian Lapierre <adrian@softproject.com.pl>
@@ -15,6 +17,19 @@ public class MyController {
     @RequestMapping("/rest/hello")
     public String hallo() {
         return "Hello world";
+    }
+    
+    @RequestMapping("/rest/person/{id}")
+    public Person loadPerson(@PathVariable("id") Integer id) {
+        
+        System.out.println("id = " + id);
+        
+        Person p = new Person();
+        
+        p.setLastName("Kowalski");
+        p.setName("Jan");
+        
+        return p;
     }
 
 }
